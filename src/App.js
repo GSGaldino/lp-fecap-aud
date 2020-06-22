db51d00494496fed23414eca4eb8806d77b23008
Gabriel Soares <gabrielsoaresii@outlook.com>
2020-06-22 14:01:54 -0300

    Changes

--- src/App.js
+++ src/App.js
@@ -3,20 +3,14 @@ import Routes from './routes';
 
 import './reset.css';
 
-import TagManager from 'react-gtm-module';
 
-const tagManagerArgs = {
-  gtmId: 'GTM-59NQXVK'
-}
 
 function App() {
   return (
     <div className="App">
-      <Routes />
+      <Routes />     
     </div>
   );
 }
 
-
-TagManager.initialize(tagManagerArgs)
 export default App;
